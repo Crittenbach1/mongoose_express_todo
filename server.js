41c8795efd2c60158a4aea0c25b69fb1bb09cea8
crittenbach1 <crittenbach@gmail.com>
2019-02-18 16:49:12 -0500

    call routes file from server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const routes = require('./routes/routes.js');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +12,8 @@ mustacheExpressInstance.cache = null;
 app.engine('mustache', mustacheExpressInstance);
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+
+app.use('/', routes);
 
 app.listen(3000, function() {
    console.log("Listening on port 3000");
